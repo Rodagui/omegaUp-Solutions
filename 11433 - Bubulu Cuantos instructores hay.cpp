3d2c661085ed295b0283983c941fe5549ca93a86
Kathia Rodríguez <39465484+Rodagui@users.noreply.github.com>
2023-03-21 20:20:45 -0600

    Add files via upload

---
+++ 11433 - Bubulu Cuantos instructores hay.cpp
@@ -0,0 +1,23 @@
+/*¿Bubulu-cuántos instructores hay? - Omegaup*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+		
+	ios_base::sync_with_stdio(0);
+	cin.tie();
+	cout.tie();
+
+	string nombre;
+	int ans = 0;
+
+	while(cin >> nombre){
+		ans++;
+	}
+
+	cout << ans;
+
+	return 0;
+}
